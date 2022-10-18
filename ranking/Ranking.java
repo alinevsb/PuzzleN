35df126cac7bb75cb32f2cce464ec7070beb14b7
danieldlf <danielddlf2003@gmail.com>
2022-10-18 04:10:56 -0300

    Limpeza

--- ranking/Ranking.java
+++ ranking/Ranking.java
@@ -1,18 +1,9 @@
 package ranking;
 
-import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
-import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.nio.file.StandardOpenOption;
-import java.util.List;
-import java.util.Scanner;
 
 public class Ranking {
     private static long tempo;
